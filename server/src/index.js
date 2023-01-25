31862b9a576030e724d6eec3213035a599ec51f2
Osbourne34 <evolkov1804@gmail.com>
2023-01-25 22:06:57 +0500

    init app

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT: ${PORT}`);
+});
